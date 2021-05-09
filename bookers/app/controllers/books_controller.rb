f41b8bcc41f2a7ee802219c9c0a74905894ff8c6
EC2 Default User <tkspp2sko@gmail.com>
2021-05-09 13:00:00 +0000

    詳細画面表示へのリンク完成

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -4,19 +4,28 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+
   end
 
 
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to "/books"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to "/books"
   end
 
