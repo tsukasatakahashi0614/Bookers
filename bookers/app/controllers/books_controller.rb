fd12085561b2fceb447dccfa7bdb0aa5408a2f13
EC2 Default User <tkspp2sko@gmail.com>
2021-05-09 09:10:06 +0000

    投稿機能作途中

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,12 +1,27 @@
 class BooksController < ApplicationController
 end
 
-
 def index
+  @books = Book.all
 end 
+
+def new
+end
 
 def show
 end 
 
 def edit
+end
+
+def create
+  book = Book.new(book_params)
+  book.save
+  redirect_to "/"
+end
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :body)
 end
