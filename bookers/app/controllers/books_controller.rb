89e3c7bef654774c79632040eab8694061f15e43
EC2 Default User <tkspp2sko@gmail.com>
2021-05-09 10:21:56 +0000

    投稿機能追加

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,27 +1,29 @@
 class BooksController < ApplicationController
-end
 
-def index
-  @books = Book.all
-end 
 
-def new
-end
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
 
-def show
-end 
 
-def edit
-end
 
-def create
-  book = Book.new(book_params)
-  book.save
-  redirect_to "/"
-end
+  def show
+  end
 
-private
+  def edit
+  end
 
-def book_params
-  params.require(:book).permit(:title, :body)
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/books"
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- bookers/app/controllers/homes_controller.rb
+++ bookers/app/controllers/homes_controller.rb
@@ -1,5 +1,28 @@
 class HomesController < ApplicationController
 end
 
-def top
+def index
+  @books = Book.all
+end 
+
+def new
+  @book = Book.new
+end
+
+def show
+end 
+
+def edit
+end
+
+def create
+  @book = Book.new(book_params)
+  @book.save
+
+end
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :body)
 end
